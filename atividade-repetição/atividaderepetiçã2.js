ce9da79adfb6d40d47c5bdd2cf42c783568e7e22
dinhaxz <deborahmendonca124@gmail.com>
2025-10-08 17:47:01 -0300

    bagunça

--- atividade-repetição/atividaderepetiçã2.js
+++
@@ -1,3 +0,0 @@
-for (let i = 101; i <= 110; i++ ){
-    console.log(i);
-}
---
+++ bagunça/atividade-repetição/ativiaderepeticão3.js
@@ -0,0 +1,8 @@
+let soma = 0; 
+
+for (let i = 0; 10; i++) {
+let numero = parseInt(prompt('digite o ${i + 1} numero:'), 10);
+soma += numero;
+}
+
+console.log("soma total:", soma);
